96251515488ce79138623fd75db4ca9a20d891ba
Cody Weaver <Cody@Codys-MacBook-Air.local>
2015-01-13 22:39:19 -0800

    Initial Commit

---
+++ Htlaeh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Htlaeh
+//
+//  Created by Cody Weaver on 1/13/15.
+//  Copyright (c) 2015 Cody Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
